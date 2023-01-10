3140a68ee0f3b17aedecbcb2ccf3a04d3442e265
Vitor Rosa <vrfrosa1@gmail.com>
2023-01-10 16:24:10 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -10,12 +10,14 @@ namespace xadrez_console.tabuleiro
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Cor cor, Tabuleiro tab)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.cor = cor;
             this.qteMovimentos = 0;
             this.tab = tab;
         }
+
+        
     }
 }
---
+++ xadrez-console/xadrez/Torre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using tabuleiro;
+using xadrez_console.tabuleiro;
+
+namespace xadrez
+{
+    internal class Torre : Peca
+    {
+        public Torre(Tabuleiro tab, Cor cor) : base(cor, tab)
+        {
+
+        }
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
