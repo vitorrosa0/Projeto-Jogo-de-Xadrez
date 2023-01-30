006d176239f81075482b3716aa1410fc671cf033
Vitor Rosa <vrfrosa1@gmail.com>
2023-01-30 16:00:51 -0300

    Correcao: mudando o nome do metodo para movimentoPossivel

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -43,7 +43,7 @@ namespace xadrez_console.tabuleiro
             return false;
         }
 
-        public bool podeMoverPara(Posicao pos)
+        public bool movimentoPossivel(Posicao pos)
         {
             return movimentosPossiveis()[pos.linha, pos.coluna];
         }
